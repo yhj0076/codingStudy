52b870668a2fd7a07a39c1c280aed27168d642ac
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-05-05 00:36:27 +0900

    [Gold III] Title: 파티, Time: 64 ms, Memory: 8228 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cs
+++ 백준/Gold/1238. 파티/파티.cs
@@ -1,77 +1,75 @@
+using System;
+using System.Collections.Generic;
+
 public class Starter
 {
+    static int N, M, X;
+    static List<(int to, int cost)>[] graph;
+    static List<(int to, int cost)>[] reverseGraph;
+
     public static void Main(string[] args)
     {
         string[] input = Console.ReadLine().Split();
-        int N = int.Parse(input[0]);
-        int M = int.Parse(input[1]);
-        int X = int.Parse(input[2]);
+        N = int.Parse(input[0]);
+        M = int.Parse(input[1]);
+        X = int.Parse(input[2]);
 
-        int[,] graph = new int[N+1,N+1];
-        
+        graph = new List<(int, int)>[N + 1];
+        reverseGraph = new List<(int, int)>[N + 1];
+
+        for (int i = 1; i <= N; i++)
+        {
+            graph[i] = new List<(int, int)>();
+            reverseGraph[i] = new List<(int, int)>();
+        }
+
         for (int i = 0; i < M; i++)
         {
             input = Console.ReadLine().Split();
-            
             int from = int.Parse(input[0]);
             int to = int.Parse(input[1]);
             int cost = int.Parse(input[2]);
-            
-            graph[from,to] = cost;
+
+            graph[from].Add((to, cost));
+            reverseGraph[to].Add((from, cost)); // 역방향 저장
         }
-        
-        int[] startX = new int[N+1];
-        int[] endX = new int[N+1];
-        
-        for (int i = 1; i <= N; i++)
-        {
-            if (i != X)
-            {
-                startX[i] = int.MaxValue;
-                endX[i] = int.MaxValue;
-            }
-        }
-        
-        // [(거리, 노드), 거리]
-        PriorityQueue<(int, int), int> pQ = new PriorityQueue<(int, int), int>();
-        pQ.Enqueue((0,X),0);
-        bool[] visited = new bool[N+1];
-        while (pQ.Count > 0)
-        {
-            (int distance, int node) element = pQ.Dequeue();
-            visited[element.node] = true;
-            for (int i = 1; i <= N; i++)
-            {
-                if (graph[element.node, i] > 0 && !visited[i])
-                {
-                    startX[i] = Math.Min(startX[i], element.distance + graph[element.node, i]);
-                    pQ.Enqueue((startX[i], i), startX[i]);
-                }
-            }
-        }
-        
-        pQ.Enqueue((0,X),0);
-        visited = new bool[N+1];
-        while (pQ.Count > 0)
-        {
-            (int distance, int node) element = pQ.Dequeue();
-            visited[element.node] = true;
-            for (int i = 1; i <= N; i++)
-            {
-                if (graph[i, element.node] > 0 && !visited[i])
-                {
-                    endX[i] = Math.Min(endX[i], element.distance + graph[i, element.node]);
-                    pQ.Enqueue((endX[i], i), endX[i]);
-                }
-            }
-        }
+
+        int[] distFromX = Dijkstra(graph, X);
+        int[] distToX = Dijkstra(reverseGraph, X);
 
         int answer = 0;
         for (int i = 1; i <= N; i++)
         {
-            answer = Math.Max(answer, startX[i] + endX[i]);
+            answer = Math.Max(answer, distFromX[i] + distToX[i]);
         }
 
         Console.WriteLine(answer);
+    }
+
+    static int[] Dijkstra(List<(int to, int cost)>[] g, int start)
+    {
+        int[] dist = new int[N + 1];
+        Array.Fill(dist, int.MaxValue);
+        dist[start] = 0;
+
+        var pq = new PriorityQueue<(int node, int dist), int>();
+        pq.Enqueue((start, 0), 0);
+
+        while (pq.Count > 0)
+        {
+            var (cur, cost) = pq.Dequeue();
+            if (cost > dist[cur]) continue;
+
+            foreach (var (next, nextCost) in g[cur])
+            {
+                if (dist[next] > dist[cur] + nextCost)
+                {
+                    dist[next] = dist[cur] + nextCost;
+                    pq.Enqueue((next, dist[next]), dist[next]);
+                }
+            }
+        }
+
+        return dist;
     }
 }
